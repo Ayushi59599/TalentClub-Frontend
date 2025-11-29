9e4bf8738f7f119c4a923ead55fb10df1c96bfad
Ayushi Yadav <Ayushi59599@gmail.com>
2025-11-29 19:44:17 +0000

    Initial project setup

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // IMPORTANT: Replace 'TalentClub-Frontend' with your EXACT GitHub Repository name
+  base: '/TalentClub-Frontend/', 
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
